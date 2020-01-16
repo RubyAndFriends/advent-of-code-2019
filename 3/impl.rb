8d345d65bbf720fae80a149e800de03397afb417
David Vrensk <david@vrensk.com>
2020-01-16 21:07:03 +0100

    Day 3: [ADD] Impl runs example 1 [ADD] Impl finds positions
    [DEL] Impl works

--- 3/impl.rb
+++ 3/impl.rb
@@ -1,7 +1,24 @@
 class Impl
-  def self.run(a, b)
+  def self.run(wire_one, wire_two)
     6
   end
-  
-  
+
+  def self.positions(wire)
+    x, y = [0, 0]
+    all = [[x,y]]
+    wire.split(",").each do |instruction|
+      direction, steps = instruction.match(/(.)(.+)/)[1..2]
+      steps = steps.to_i
+      steps.times do
+        case direction
+        when "U" then y+=1
+        when "D" then y-=1
+        when "R" then x+=1
+        when "L" then x-=1
+        end
+        all << [x,y]
+      end
+    end
+    all
+  end
 end
--- 3/impl_spec.rb
+++ 3/impl_spec.rb
@@ -1,7 +1,27 @@
 require_relative "impl"
 
 describe Impl do
-  it "works" do
+  it "runs example 1" do
     expect(Impl.run("R8,U5,L5,D3", "U7,R6,D4,L4")).to eq 6
+  end
+
+  # it "runs example 2" do
+  #   expect(Impl.run("R75,D30,R83,U83,L12,D49,R71,U7,L72", "U62,R66,U55,R34,D71,R55,D58,R83")).to eq 159
+  # end
+
+  it "finds positions" do
+    expect(Impl.positions("U1,L2,D3,R4")).to eq [
+      [0, 0],
+      [0, 1],
+      [-1, 1],
+      [-2, 1],
+      [-2, 0],
+      [-2, -1],
+      [-2, -2],
+      [-1, -2],
+      [0, -2],
+      [1, -2],
+      [2, -2],
+    ]
   end
 end
