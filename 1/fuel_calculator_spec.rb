57588ea50b1e2b299813688e06aaf40f1f437fe6
David Vrensk <david@vrensk.com>
2020-01-16 19:31:28 +0100

    Day 1: [ADD] FuelCalculator For a mass of 14, dividing by 3 and rounding down still yields 4, so the fuel required is also 2

--- 1/fuel_calculator_spec.rb
+++ 1/fuel_calculator_spec.rb
@@ -4,4 +4,8 @@ describe FuelCalculator do
   it "For a mass of 12, divide by 3 and round down to get 4, then subtract 2 to get 2" do
     expect(described_class.calculate(12)).to eq 2
   end
+
+  it "For a mass of 14, dividing by 3 and rounding down still yields 4, so the fuel required is also 2" do
+    expect(described_class.calculate(14)).to eq 2
+  end
 end
